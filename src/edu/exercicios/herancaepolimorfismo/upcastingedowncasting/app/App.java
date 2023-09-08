7fbc94412d9af9ae7d99769c07cd785254e128b5
Wesley Pereira <wlypereiraa@gmail.com>
2023-09-08 20:30:10 -0300

    ...

--- src/edu/exercicios/herancaepolimorfismo/upcastingedowncasting/app/App.java
+++ src/edu/exercicios/herancaepolimorfismo/upcastingedowncasting/app/App.java
@@ -11,10 +11,12 @@ public class App {
 
     public static void upcastingDowncasting() {
         Conta conta = new Conta(1001, "Wesley", 1000.0);
+        conta.depositar(400.0);
         ContaEmpresarial contaEmpresarial = new ContaEmpresarial(1002, "Camila", 0.0, 2000.0);
 
         // Upcasting
         Conta conta2 = contaEmpresarial;
+        conta2.depositar(400.0);
         Conta conta3 = new ContaEmpresarial(1003, "Neusa", 0.0, 500.0);
         Conta conta4 = new ContaPoupanca(1004, "Danielle", 0.0, 0.01);
 
