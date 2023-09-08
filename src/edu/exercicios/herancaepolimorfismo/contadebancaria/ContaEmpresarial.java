39cd5f125311d6949adc55262ec536034e6d1abb
Wesley Pereira <wlypereiraa@gmail.com>
2023-09-07 23:26:46 -0300

    exercicio conta bancaria

---
+++ src/edu/exercicios/herancaepolimorfismo/contadebancaria/ContaEmpresarial.java
@@ -0,0 +1,24 @@
+package edu.exercicios.herancaepolimorfismo.contadebancaria;
+
+public class ContaEmpresarial extends Conta {
+    private Double limiteEmprestimo;
+
+    public ContaEmpresarial(Integer numero, String titular, Double saldo, Double limiteEmprestimo) {
+        super(numero, titular, saldo);
+        this.limiteEmprestimo = limiteEmprestimo;
+    }
+
+    public Double getLimiteEmprestimo() {
+        return this.limiteEmprestimo;
+    }
+
+    public void setLimiteEmprestimo(Double limiteEmprestimo) {
+        this.limiteEmprestimo = limiteEmprestimo;
+    }
+
+    public void emprestimo(Double valor) {
+        if (valor <= this.limiteEmprestimo) {
+            this.saldo += valor - 10.0;
+        }
+    }
+}
