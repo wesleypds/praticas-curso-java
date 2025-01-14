93e76f286e0d98f8c01906115dd3d77919726c21
wesleypds <jubileu336@gmail.com>
2025-01-14 14:31:56 -0300

    :recycle: refactor: refatorando solução boa

--- src/edu/exercicios/tratamentoexcecoes/excecoespersonalizadas/solucaoboa/model/entidades/Reserva.java
+++ src/edu/exercicios/tratamentoexcecoes/excecoespersonalizadas/solucaoboa/model/entidades/Reserva.java
@@ -1,25 +1,22 @@
 package edu.exercicios.tratamentoexcecoes.excecoespersonalizadas.solucaoboa.model.entidades;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import edu.exercicios.tratamentoexcecoes.excecoespersonalizadas.solucaoboa.model.excecoes.ExcecaoPersonalizada;
 
 public class Reserva {
     private Integer numeroQuarto;
-    private Date checkin;
-    private Date checkout;
+    private LocalDate checkin;
+    private LocalDate checkout;
 
-    private static SimpleDateFormat dateFormatada = new SimpleDateFormat("dd/MM/yyyy");
+    private static DateTimeFormatter dateFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public Reserva() {
     }
 
-    public Reserva(Integer numeroQuarto, Date checkin, Date checkout) throws ExcecaoPersonalizada {
-        if (!checkout.after(checkin)) {
-            throw new ExcecaoPersonalizada("Erro na reserva: A data de Check-out tem que ser maior que a data de Check-in.");
-        }
+    public Reserva(Integer numeroQuarto, LocalDate checkin, LocalDate checkout) throws ExcecaoPersonalizada {
+        validacoes(checkin, checkout);
         this.numeroQuarto = numeroQuarto;
         this.checkin = checkin;
         this.checkout = checkout;
@@ -33,29 +30,35 @@ public class Reserva {
         this.numeroQuarto = numeroQuarto;
     }
 
-    public Date getCheckin() {
+    public LocalDate getCheckin() {
         return checkin;
     }
 
-    public Date getCheckout() {
+    public LocalDate getCheckout() {
         return checkout;
     }
 
-    public Long duracao() {
-        long dif = checkout.getTime() - checkin.getTime();
-        return TimeUnit.DAYS.convert(dif, TimeUnit.MILLISECONDS);
+    public Integer duracao() {
+        return checkout.getDayOfYear() - checkin.getDayOfYear();
     }
 
-    public void atualizaDatas(Date checkin, Date checkout) throws ExcecaoPersonalizada {
-        Date agora = new Date();
-        if (checkout.before(agora) || checkin.before(agora)) {
-            throw new ExcecaoPersonalizada("Erro na reserva: As datas de reserva devem ser datas futuras.");
-        }
-        if (!checkout.after(checkin)) {
-            throw new ExcecaoPersonalizada("Erro na reserva: A data de Check-out tem que ser maior que a data de Check-in.");
-        }
+    public void atualizaDatas(LocalDate checkin, LocalDate checkout) throws ExcecaoPersonalizada {
+        validacoes(checkin, checkout);
         this.checkin = checkin;
         this.checkout = checkout;
+    }
+
+    public void validacoes(LocalDate checkin, LocalDate checkout) throws ExcecaoPersonalizada {
+        LocalDate agora = LocalDate.now();
+        if (checkin.isBefore(agora)) {
+            throw new ExcecaoPersonalizada("Erro na reserva: A data de Checkin deve ser igual a hoje ou posterior.");
+        }
+        if (checkout.isBefore(agora)) {
+            throw new ExcecaoPersonalizada("Erro na reserva: A data de Checkout deve ser posterior a hoje.");
+        }
+        if (!checkout.isAfter(checkin)) {
+            throw new ExcecaoPersonalizada("Erro na reserva: A data de Check-out tem que ser maior que a data de Check-in.");
+        }
     }
 
     @Override
