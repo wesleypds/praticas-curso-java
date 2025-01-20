8a7940d12ce80bb70ff1c38db29993aa9754ebf2
wesleypds <wlypereiraa@gmail.com>
2025-01-20 13:42:18 -0300

    métodos genéricos

---
+++ src/edu/exercicios/genericssetmap/genericodelimitado/app/App.java
@@ -0,0 +1,35 @@
+package edu.exercicios.genericssetmap.genericodelimitado.app;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.exercicios.genericssetmap.genericodelimitado.model.Produto;
+import edu.exercicios.genericssetmap.genericodelimitado.services.CalculoService;
+
+public class App {
+    public static void main(String[] args) {
+
+        String path = "C:\\Workspace\\Cursos\\Curso Java\\Exercicios-vscode\\exercicios-java-curso\\src\\edu\\exercicios\\genericssetmap\\genericodelimitado\\arquivos\\produtos.csv";
+
+        List<Produto> produtos = new ArrayList<>();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] valores = line.split(",");
+                produtos.add(new Produto(valores[0], Double.parseDouble(valores[1])));
+                line = br.readLine();
+            }
+
+            Produto p = CalculoService.max(produtos);
+            System.out.println("Maior valor: ");
+            System.out.println(String.format("%s, %.2f", p.getNome(), p.getPreco()));
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
