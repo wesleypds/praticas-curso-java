7018d7d15d9d46db795ac06b2e9021b90353c540
wesleypds <wlypereiraa@gmail.com>
2025-01-21 19:43:34 -0300

    estrutura de dados set com métodos hashCode e equals do objeto Usuario

---
+++ src/edu/exercicios/genericssetmap/registrodelog/models/Usuario.java
@@ -0,0 +1,54 @@
+package edu.exercicios.genericssetmap.registrodelog.models;
+
+import java.time.ZonedDateTime;
+
+public class Usuario {
+    private String nome;
+    private ZonedDateTime data;
+
+    public Usuario(String nome, ZonedDateTime data) {
+        this.nome = nome;
+        this.data = data;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ZonedDateTime getData() {
+        return data;
+    }
+
+    public void setData(ZonedDateTime data) {
+        this.data = data;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+}
