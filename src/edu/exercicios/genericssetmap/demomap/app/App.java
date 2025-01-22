df346601bd5cc48de278b8e66743cd6df8f0d1bf
wesleypds <wlypereiraa@gmail.com>
2025-01-21 22:23:31 -0300

    demonstração do map

---
+++ src/edu/exercicios/genericssetmap/demomap/app/App.java
@@ -0,0 +1,26 @@
+package edu.exercicios.genericssetmap.demomap.app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.exercicios.genericssetmap.demomap.models.Produto;
+
+public class App {
+    public static void main(String[] args) {
+        
+        Map<Produto, Double> cookies = new HashMap<>();
+
+        Produto p1 = new Produto("Tv", 900.0);
+        Produto p2 = new Produto("Notebook", 1200.0);
+        Produto p3 = new Produto("Tablet", 400.0);
+
+        cookies.put(p1, 900.0);
+        cookies.put(p2, 1200.0);
+        cookies.put(p3, 400.0);
+
+        Produto p4 = new Produto("Tv", 900.0);
+
+        System.err.println("Contém a chave 'p4': " + cookies.containsKey(p4));
+
+    }
+}
